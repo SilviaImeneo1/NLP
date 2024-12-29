5454032cf18ddb93a34443c72034588f56c10134
SilviaImeneo1 <93650018+SilviaImeneo1@users.noreply.github.com>
2024-12-29 10:36:11 +0100

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,74 +1,75 @@
-from ast import parse
 import os
 from argparse import ArgumentParser
 import tensorflow as tf
 import pandas as pd
-from tensorflow.python.keras.layers import embeddings 
-from data import *
-from constant import *
 import numpy as np
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-from constant import *
+from data import Dataset
+
 if __name__ == "__main__":
+    # Parser degli argomenti
     parser = ArgumentParser()
-    home_dir = os.getcwd() 
-    parser.add_argument("--smallCharCNN-folder", default="smallCharCNN", type=str)
-    parser.add_argument("--vocab-folder", default= '{}/saved_vocab/CharCNN/'.format(home_dir), type= str)
-    parser.add_argument("--largeCharCNN-folder", default="largeCharCNN", type=str)
-    parser.add_argument("--test-file", default="test.csv", type=str)
-    parser.add_argument("--model", default="small", type=str)
-    parser.add_argument("--result-file", default="result.csv", type=str)
+    home_dir = os.getcwd()
+    parser.add_argument("--smallCharCNN-folder", default="smallCharCNN", type=str, help="Directory del modello Small-CharCNN")
+    parser.add_argument("--largeCharCNN-folder", default="largeCharCNN", type=str, help="Directory del modello Large-CharCNN")
+    parser.add_argument("--vocab-folder", default=f'{home_dir}/saved_vocab/CharCNN/', type=str, help="Directory del vocabolario salvato")
+    parser.add_argument("--test-file", default="test.csv", type=str, help="File CSV contenente i dati di test")
+    parser.add_argument("--model", default="small", type=str, choices=["small", "large"], help="Modello da utilizzare: 'small' o 'large'")
+    parser.add_argument("--result-file", default="result.csv", type=str, help="File CSV per salvare i risultati")
     args = parser.parse_args()
 
+    # Informazioni di benvenuto
     print('---------------------Welcome to CharCNN-------------------')
     print("Team Leader")
     print("1. Github: hoangcaobao")
     print("Team member")
     print('1. Github: Nguyendat-bit')
     print('2. Github: aestheteeism')
-    print('---------------------------------------------------------------------')
-    print('Training CharCNN model with hyper-params:') 
+    print('----------------------------------------------------------')
+    print('Caricamento dei parametri di configurazione:')
     for i, arg in enumerate(vars(args)):
-        print('{}. {}: {}'.format(i, arg, vars(args)[arg]))
-    print('===========================')
+        print(f'{i+1}. {arg}: {vars(args)[arg]}')
+    print('==========================================================')
 
-    # Loading Models 
-    # Small-CharCNN model
-    if(args.model=="small"):
+    # Caricamento del modello
+    print('============= Caricamento del modello =============')
+    if args.model == "small":
         model = tf.keras.models.load_model(args.smallCharCNN_folder)
-    # Large-CharCNN model
     else:
         model = tf.keras.models.load_model(args.largeCharCNN_folder)
+    print(f'Modello {args.model} caricato con successo.')
 
-    # Loading Tokenizer
-    print('=============Loading Tokenizer================')
-    print('Begin...')
-    dataset = Dataset(vocab_folder= args.vocab_folder)
-
+    # Caricamento del tokenizer
+    print('============= Caricamento del tokenizer ===========')
+    dataset = Dataset(vocab_folder=args.vocab_folder)
     label_dict = dataset.label_dict
-    print('Done!!!')
+    print('Tokenizer caricato con successo.')
 
-    # Load test sentences 
-    sentence = pd.read_csv(args.test_file)
-    sentence.columns = ['sentence']
-    sentence = np.array(sentence['sentence'])
-    test = [dataset.preprocess_data(i) for i in sentence]
-    test = dataset.tokenizer.texts_to_sequences(test)
-    test = pad_sequences(test, maxlen= model.layers[0].input_length, padding=padding)
-    
-    # Preidct
-    print("================Predicting================")
-    predict=model.predict(test)
-    predict=np.argmax(predict, axis=1)
-    predict=predict.astype("O")
+    # Caricamento e preprocessing dei dati di test
+    print('============= Preprocessing dei dati di test ======')
+    try:
+        test_data = pd.read_csv(args.test_file, header=None, names=['sentence'])
+    except FileNotFoundError:
+        print(f"Errore: Il file {args.test_file} non esiste. Verifica il percorso e riprova.")
+        exit(1)
 
-    #Decode predict
-    label_dict = dict((i,l) for l,i in label_dict.items())
-    for label_num in label_dict.keys():
-        predict[predict == label_num] = label_dict[label_num]
-    #Save to csv
-    data=np.column_stack((sentence, predict))
-    df=pd.DataFrame(data, columns=['sentence', 'label'])
-    df.to_csv(args.result_file, index=False)
-    print("End Predicting. Now your result will be in {}".format(args.result_file))
+    sentences = test_data['sentence'].values
+    preprocessed_sentences = [dataset.preprocess_data(s) for s in sentences]
+    tokenized_sentences = dataset.tokenizer.texts_to_sequences(preprocessed_sentences)
+    max_len = model.input_shape[1]  # Lunghezza massima dall'input del modello
+    padded_sentences = pad_sequences(tokenized_sentences, maxlen=max_len, padding='post')
 
+    # Predizione
+    print('============= Predizione ==========================')
+    predictions = model.predict(padded_sentences)
+    predicted_labels = np.argmax(predictions, axis=1)
+
+    # Decodifica delle predizioni
+    reverse_label_dict = {i: label for label, i in label_dict.items()}
+    decoded_labels = [reverse_label_dict[label] for label in predicted_labels]
+
+    # Salvataggio dei risultati
+    print('============= Salvataggio dei risultati ===========')
+    result_df = pd.DataFrame({'sentence': sentences, 'label': decoded_labels})
+    result_df.to_csv(args.result_file, index=False)
+    print(f"Predizioni salvate in {args.result_file}")
